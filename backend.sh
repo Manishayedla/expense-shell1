fc0a6255bd9b64a317dfa965cfb494e574c12a05
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-27 14:41:59 -0400

    run

--- backend.sh
+++ backend.sh
@@ -6,55 +6,55 @@ check_root
 #when you're calling function it shouldnt have () brackets
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling default nodejs"
+#VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs:20 version"
+#VALIDATE $? "Enabling nodejs:20 version"
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
+#VALIDATE $? "Installing nodejs"
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
 useradd expense &>>$LOGFILE
-VALIDATE $? "Creating expense user"
+#VALIDATE $? "Creating expense user"
 else
 echo -e "Expense user already created.. $Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "Creating app directory"
+#VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading backend code"
+#VALIDATE $? "Downloading backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
+#VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "Installing Nodejs dependencies"
+##VALIDATE $? "Installing Nodejs dependencies"
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "Copied backend dervice"
+#VALIDATE $? "Copied backend dervice"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon backend"
+#VALIDATE $? "Daemon backend"
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
+#VALIDATE $? "Starting backend"
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
+#VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Client"
+#VALIDATE $? "Installing MySQL Client"
 
 mysql -h db.manishadaws.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema loading"
+#VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restarting Backend"
+#VALIDATE $? "Restarting Backend"
 
 
 
